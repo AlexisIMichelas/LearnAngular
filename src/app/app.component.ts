4ca38012fcb08f1a994d0dfb575a25b5ea1cc465
AMichelas <120266869+AMichelas@users.noreply.github.com>
2024-04-19 10:26:22 +0200

    work on router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private name: string = 'Alexis';
-  public title: string = `Bienvenue sur le site de ${this.name} ! `;
+  name: string = 'Alexis';
+  title: string = `Bienvenue sur le site de ${this.name} !`;
 }
