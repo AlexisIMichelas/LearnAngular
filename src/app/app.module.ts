9e27d98459ea08672b4ec8272a130c849b3ac597
AMichelas <120266869+AMichelas@users.noreply.github.com>
2024-04-23 14:41:25 +0200

    output ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { MyFormComponent } from './my-form/my-form.component';
 import { DeveloperComponent } from './developer/developer.component';
 import { SkillComponent } from './skill/skill.component';
+import { CreateOnomatopiaComponent } from './create-onomatopia/create-onomatopia.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { SkillComponent } from './skill/skill.component';
     SignUpComponent,
     MyFormComponent,
     DeveloperComponent,
-    SkillComponent
+    SkillComponent,
+    CreateOnomatopiaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/create-onomatopia/create-onomatopia.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-create-onomatopia',
+  templateUrl: './create-onomatopia.component.html',
+  styleUrls: ['./create-onomatopia.component.scss']
+})
+export class CreateOnomatopiaComponent {
+
+  newOnomatopia: string = '';
+
+  @Output() sendOnomatopiaToParent: EventEmitter<string> = new EventEmitter();
+
+  createOnomatopia(): void {
+    if (this.newOnomatopia.trim() !== '') {
+      this.sendOnomatopiaToParent.emit(this.newOnomatopia);
+      this.newOnomatopia = ''; // Réinitialiser le champ d'entrée après l'envoi
+    }
+  }
+}
